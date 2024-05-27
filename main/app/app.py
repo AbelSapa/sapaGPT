2d8c21572069a65399f2b2d0d8ea4727c2c24454
AbelSapa <a.sturm@sapalomera.cat>
2024-05-27 18:04:58 +0200

    FIX: Control de error, en el momento de generar la respuesta

--- main/app/app.py
+++ main/app/app.py
@@ -11,6 +11,8 @@ def get_bot_response():
     user_input = request.args.get('msg')
     #Cridem al subprocess (model amb el input)
     result = subprocess.run(['python', 'model/SapaGPT/gpt2.py', user_input], capture_output=True, text=True)
+    if result.returncode != 0:
+        return jsonify("Error en el model de generació de text.")
     #CompletedProcess(args=['python', 'model/picoGPT/gpt2.py', 'hello bot how are you'], returncode=0, stdout=', a former member of the National Assembly, said he was "very disappointed" by the decision.\n', stderr='0.01s - Debugger warning: It seems that frozen modules are being used, which may\n0.00s - make the debugger miss breakpoints. Please pass -Xfrozen_modules=off\n0.00s - to python to disable frozen modules.\n0.00s - Note: Debugging will proceed. Set PYDEVD_DISABLE_FILE_VALIDATION=1 to disable this validation.\n')
     response = result.stdout.strip()
     return jsonify(response)
